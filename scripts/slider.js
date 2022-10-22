66e4c3b90b56e2181b53ea50f34770786b945fdc
Andrey Kuchebo <53372722+number3141@users.noreply.github.com>
2022-10-22 22:48:56 +0700

    Добавлен слайдер

---
+++ scripts/slider.js
@@ -0,0 +1,7 @@
+$('.slid').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      prevArrow: '<div class="prev"></div>',
+      nextArrow: '<div class="next"></div>',
+});
